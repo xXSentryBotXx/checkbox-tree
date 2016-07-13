ed2667ce256418c8b6be630924ca39bde4f6f2b5
unknown <luis.davila.campos@gmail.com>
2016-07-13 17:26:34 -0500

    Added some style to the example.

--- js/main.js
+++ js/main.js
@@ -57,14 +57,20 @@ var data = [{
 
 var elem = document.getElementById('tree-plugin'); //Element to append tree
 var tree = new CheckBoxTree(data, elem); //Creating a new Checkbox Tree.
-var results = document.getElementById('plugin-results');
+var resultArray = document.getElementById('array');
+var resultObject = document.getElementById('object');
 var pArray = document.createElement('p');
-var pObject = document.createElement('p');
+pArray.id = 'result-array';
+var pObject = document.createElement('pre');
+
+resultArray.appendChild(pArray);
+resultObject.appendChild(pObject);
+
+function callback () {
+	pArray.innerText = tree.value.selectedsArray; //Geting the array of selected checkboxes
+	pObject.innerText = JSON.stringify(tree.value.dataObject, null, 2); //Getting the Javascript Object with detailed info.
+}
 
 //Listener added to show results.
-tree.treeElement.addEventListener('change', function () {
-	pArray.innerText = 'Selecteds Array: ' + tree.value.selectedsArray; //Geting the array of selected checkboxes
-	pObject.innerText = 'Selecteds Object: ' + JSON.stringify(tree.value.dataObject); //Getting the Javascript Object with detailed info.
-});
-results.appendChild(pArray);
-results.appendChild(pObject);
+tree.treeElement.addEventListener('change', callback);
+callback();
