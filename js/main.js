cc5bcc2af13ac085d12b4f93114039e160304ba6
unknown <luis.davila.campos@gmail.com>
2016-07-24 23:18:25 -0500

    Added some functionality and style to the tabs menu.

--- js/main.js
+++ js/main.js
@@ -156,3 +156,21 @@
 	tree.treeElement.addEventListener('change', callback);
 	callback();
 })()
+
+function openMethod(evt, method) {
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(method).style.display = "block";
+    evt.currentTarget.className += " active";
+}
