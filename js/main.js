74a1f816b92f4369d98a43e06d8194f7cd1fc524
unknown <luis.davila.campos@gmail.com>
2016-07-26 22:46:58 -0500

    Changed some characters to match the code.

--- js/main.js
+++ js/main.js
@@ -173,14 +173,14 @@ function openMethod(evt, method) {
 
     var i, tabcontent, tablinks, elems;
 
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName('tabcontent');
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = 'none';
     }
 
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.getElementsByClassName('tablinks');
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(' active', '');
     }
 
     elems = document.getElementsByClassName(method);
@@ -188,7 +188,7 @@ function openMethod(evt, method) {
     console.log(elems);
 
     for (i = 0; i < elems.length; i++) {
-    	elems[i].style.display = "block";
+    	elems[i].style.display = 'block';
     }
-    evt.currentTarget.className += " active";
+    evt.currentTarget.className += ' active';
 }
